6442af30cd2663dadd5bcf8ab4b78b9fb1228ec5
andrealezy <andrealezy1@gmail.com>
2026-02-07 00:27:33 -0300

    crição do header do landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Header } from './components/header/Header'
 
 import './App.css'
 
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,1 +1,24 @@
+import { MessageCircle } from 'lucide-react';
+import { dataContent } from '../../../data'
+import './header.css'
+import '../../index.css'
 
+export const Header = () => {
+  return (
+    <>
+      <header className='container'>
+        <div className='limited'>
+          <h1 className='logo'>{dataContent.header.logo}</h1>
+          <nav className='nav-header'>
+            <ul>
+              {dataContent.header.ullist.map((li, index) => (
+                <ul key={index}>{li.list}</ul>
+              ))}
+            </ul>
+          </nav>
+          <button id='btnheader' className='btndefault'>  <MessageCircle/> {dataContent.header.btnheader}</button>
+        </div>
+      </header>
+    </>
+  )
+}
