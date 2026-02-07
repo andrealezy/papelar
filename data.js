a22b99f3921bd1c861ade7e47efeafcf985feb32
andrealezy <andrealezy1@gmail.com>
2026-02-06 23:36:22 -0300

    Primeiro commit projeto papelaria papelart

---
+++ data.js
@@ -0,0 +1,20 @@
+export const dataContent = {
+  header:{
+    logo: 'papelart',
+    ullist: [
+      {list: 'Serviços'},
+      {list: 'Pré-Orçamento'},
+      {list: 'Contato'}
+    ],
+    btnheader: 'Chamamar no WhatsApp',
+  },
+  
+  banner: {
+    title:'Impressões e papelaria com rapidez e capricho',
+    description: 'Impressão, xerox, fotos 3x4, logomarca e muito mais — peça seu orçamento agora.',
+    btns:[
+      {btntext: 'Pedir orçamento no WhatsApp'},
+      {btntext: 'Montar pré-orçamento'}
+    ],
+  },
+}
